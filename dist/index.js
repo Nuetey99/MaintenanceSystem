2fe2761b8fd0555a7d5c39416d44514eca8c1295
nuetey99 <nueteyn03@gmail.com>
2024-08-27 01:09:01 +0000

    build

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,15 @@
 "use strict";
-const greet = (name) => {
-    return `Hello, ${name}!`;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-console.log(greet('World'));
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
+const database_1 = require("./database");
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+(0, database_1.connectDB)();
+const PORT = process.env.PORT || 5000;
+app.use(express_1.default.json());
+app.use('/api/auth', auth_routes_1.default);
---
+++ dist/models/user.models.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const UserSchema = new mongoose_1.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+const User = (0, mongoose_1.model)('Users', UserSchema);
+exports.default = User;
